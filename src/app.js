379bb044d43f12e05e8d0172155126577fa34256
wilberpulido <pulidowilber@gmail.com>
2021-03-10 22:37:51 -0300

    cambie version en json

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const express = require("express");
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const morgan = require('morgan')
 const bodyParser = require('body-parser');
 const cors = require("cors");
 
